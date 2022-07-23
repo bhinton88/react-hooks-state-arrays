42781f1905d3a6eda86533c5bd0de38ebda20c5c
Bobby Hinton <bob.g.hint@gmail.com>
2022-07-23 17:37:22 -0500

    commit tree cbe354834f59d3f1009e4a4acef819ec813bf7f7

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -4,16 +4,45 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
 
+  // foods is the original variable which is set to our spicyFoods array
+
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
+    const updatedFoods = [...foods, newFood]
+    setFoods(updatedFoods)
     console.log(newFood);
+    console.log(foods)
   }
 
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleIclick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
+
+  function handleIclick(id) {
+    const newFoodsList = foods.map(food => {
+      if(food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        } // used the spread operator to add the food object to the array with an updated
+        // key value pair due to the click 
+      } else {
+        return food
+        // returns the original food object if the ID does not match 
+      }
+    })
+    setFoods(newFoodsList)
+
+  }
+
+  // function which removed items from the foods array when they were clicked on
+  // function handleIclick(id) {
+  //   const removedFoods = foods.filter(food => food.id !== id)
+  //   setFoods(removedFoods)
+    
+  // }
 
   return (
     <div>
